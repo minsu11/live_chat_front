cdbd2c82c51af4184aa9a5c357b6b8d21bac215a
minsu <zmfjtnl1@naver.com>
2026-04-24 02:42:09 +0900

    [Feat] 가상 스크롤러 패키지 install

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import axios from "axios";
 import router from "./routers"
 import VueCookies from "vue-cookies"
 import VueDatePicker from "@vuepic/vue-datepicker"
+import VueVirtualScroller from 'vue-virtual-scroller'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 import '@vuepic/vue-datepicker/dist/main.css'
 
 
@@ -13,6 +15,7 @@ loadFonts()
 const app = createApp(App)
 app.use(router)
 app.use(vuetify)
+app.use(VueVirtualScroller)
 app.component('VueDatePicker', VueDatePicker)
 app.use(VueCookies, {
 
